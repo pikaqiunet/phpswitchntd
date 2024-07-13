4185e0b50d67a622960d70886dc96e2c1d7961db
小杰QAQ <1243919203@qq.com>
2024-07-14 01:10:07 +0800

    Update Api.php

--- app/controller/Api.php
+++ app/controller/Api.php
@@ -1,33 +1,34 @@
 <?php
+
 namespace app\controller;
+
 use think\facade\Log;
 use think\Request;
+
 class Api
 {
 
-    public function message(Request $request){
-        
-        Log::write('post前');
-        if($request->isPost()){
+    public function message(Request $request)
+    {
+
+
+        if ($request->isPost()) {
             Log::write('post后');
-            $data=$request->getInput();
-            $obj=json_decode($data);
-            $ToUserName=$obj->ToUserName;
-            $FromUserName=$obj->FromUserName;
-            $CreateTime=$obj->CreateTime;
-            $MsgType=$obj->MsgType;
-            $Content=$obj->Content;
-                          
-           return response(json_encode([
-             "ToUserName"=>$FromUserName,
-             "ToUserName"=>$ToUserName,
-             "CreateTime"=>$CreateTime,
-             "MsgType"=>$MsgType,
-             "Content"=>"回复：".$Content
-           ]));   
-
+            $data = $request->getInput();
+            $obj = json_decode($data);
+            $ToUserName = $obj->ToUserName;
+            $FromUserName = $obj->FromUserName;
+            $CreateTime = $obj->CreateTime;
+            $MsgType = $obj->MsgType;
+            $Content = $obj->Content;
+            Log::write('post后');
+            return response(json_encode([
+                "ToUserName" => $FromUserName,
+                "FromUserName" => $ToUserName,
+                "CreateTime" => $CreateTime,
+                "MsgType" => $MsgType,
+                "Content" => "回复：" . $Content
+            ]));
         }
-        
     }
-
 }
