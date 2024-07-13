38c886f5ed80a0b509c680d89e6f2235e1c064bd
小杰QAQ <1243919203@qq.com>
2024-07-14 02:26:31 +0800

    init

--- app/controller/Api.php
+++ app/controller/Api.php
@@ -7,13 +7,21 @@ use think\Request;
 
 class Api
 {
-
-    public function message(Request $request)
+    //小程序密码获取接口
+    public function code(Request $request)
     {
 
+        if ($request->isGet()) {
+            //从数据库调用密码
+            return json(["code"=>"1234"]);
 
+        }
+    }
+
+    //用户消息回复
+    public function message(Request $request)
+    {
         if ($request->isPost()) {
-            Log::write('post后');
             $data = $request->getInput();
             $obj = json_decode($data);
             $ToUserName = $obj->ToUserName;
@@ -21,13 +29,29 @@ class Api
             $CreateTime = $obj->CreateTime;
             $MsgType = $obj->MsgType;
             $Content = $obj->Content;
-            return json([
-                "ToUserName" => $FromUserName,
-                "FromUserName" => $ToUserName,
-                "CreateTime" => $CreateTime,
-                "MsgType" => $MsgType,
-                "Content" => "回复：" . $Content.'www.switchntd.com'
-            ]);
+            if ($MsgType=="text") {
+
+                //查询数据库业务逻辑
+
+                //<a href="https://github.com/pikaqiunet/phpswitchntd">pikaqiunet/phpswitchntd</a>
+                return json([
+                    "ToUserName" => $FromUserName,
+                    "FromUserName" => $ToUserName,
+                    "CreateTime" => $CreateTime,
+                    "MsgType" => $MsgType,
+                    "Content" => "“".$Content."”"."的查询结果：" .'<a href="https://wwww.switchntd.com">'.$Content.'</a>'
+                ]);
+
+            }else{
+                return json([
+                    "ToUserName" => $FromUserName,
+                    "FromUserName" => $ToUserName,
+                    "CreateTime" => $CreateTime,
+                    "MsgType" => "text",
+                    "Content" => "抱歉，暂不支持除文字以外的消息类型回复~"
+                ]);
+            }
+
         }
     }
 }
