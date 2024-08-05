1c2a75666604fd8684b88adb742b302f2e857065
小杰QAQ <1243919203@qq.com>
2024-08-05 18:55:35 +0800

    Update Api.php

--- app/controller/Api.php
+++ app/controller/Api.php
@@ -17,6 +17,45 @@ class Api extends Base
             return json(["code" => $server_code]);
         }
     }
+
+    private function curl_request($url, $post = array(), $cookie = '', $returnCookie = 0)
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)');
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($curl, CURLOPT_AUTOREFERER, 1);
+        curl_setopt($curl, CURLOPT_REFERER, "http://XXX");
+        if ($post) {
+            curl_setopt($curl, CURLOPT_POST, 1);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($post));
+        }
+        if ($cookie) {
+            curl_setopt($curl, CURLOPT_COOKIE, $cookie);
+        }
+        curl_setopt($curl, CURLOPT_HEADER, $returnCookie);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 1);  //ssl 这两行代码是为了能走https的请求,http请求放着也没有影响
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE); //ssl 这两行代码是为了能走https的请求,http请求放着也没有影响
+        $data = curl_exec($curl);
+        if (curl_errno($curl)) {
+            return curl_error($curl);
+        }
+        curl_close($curl);
+        if ($returnCookie) {
+            list($header, $body) = explode("\r\n\r\n", $data, 2);
+            preg_match_all("/Set\-Cookie:([^;]*);/", $header, $matches);
+            $info['cookie']  = substr($matches[1][0], 1);
+            $info['content'] = $body;
+            return $info;
+        } else {
+            return $data;
+        }
+    }
+
+
+
     //用户消息回复
     public function message(Request $request)
     {
@@ -33,16 +72,35 @@ class Api extends Base
             $FromUserName = $obj->FromUserName;
             $CreateTime = $obj->CreateTime;
             $MsgType = $obj->MsgType;
-            
+
+
 
             if ($MsgType == "event") {
-                return json([
+                //主动
+                //$data= $this->curl_request("http://api.weixin.qq.com/cgi-bin/message/custom/send",["touser"=> $FromUserName,"msgtype"=>"text","text"=>["content"=>"文本消息"]]);
+                //    return json([
+                //     "ToUserName" => $FromUserName,
+                //     "FromUserName" => $ToUserName,
+                //     "CreateTime" => $CreateTime,
+                //     "MsgType" => "text",
+                //     "Content" => $data;
+                // ]);
+
+
+                //被动
+                return json([[
                     "ToUserName" => $FromUserName,
                     "FromUserName" => $ToUserName,
                     "CreateTime" => $CreateTime,
                     "MsgType" => "text",
                     "Content" => "感谢您的关注，请点击左下角“使用说明”查看使用方式~"
-                ]);
+                ], [
+                    "ToUserName" => $FromUserName,
+                    "FromUserName" => $ToUserName,
+                    "CreateTime" => $CreateTime,
+                    "MsgType" => "text",
+                    "Content" => "感谢您的关注，请点击左下角“使用说明”查看使用方式~"
+                ]]);
                 exit;
             }
             if ($MsgType != "text") {
